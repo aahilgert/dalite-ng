953aca6d9e157e508e4f9d6a953ae33697edc088
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2020-09-04 10:31:04 -0400

    Add celery and redis services

--- dalite/settings.py
+++ dalite/settings.py
@@ -410,6 +410,12 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_ACKS_LATE = True
 CELERYD_PREFETCH_MULTIPLIER = 1
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER_URL", "redis://localhost:6379/0"
+)
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_RESULT_BACKEND", "redis://localhost:6379/0"
+)
 
 # CSP
 CSP_DEFAULT_SRC = ["'self'", "*.mydalite.org"]
