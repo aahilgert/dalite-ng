9dc856d32f58d2397295dd2abd6bce21c87e3508
jsumnerPhD <jonathon.r.e.sumner@gmail.com>
2020-11-28 16:57:59 -0500

    gunicorn settings... WIP on functional tests

--- functional_tests/teacher/test_browse_db.py
+++ functional_tests/teacher/test_browse_db.py
@@ -3,7 +3,6 @@ import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -11,13 +10,18 @@ from functional_tests.fixtures import *  # noqa
 from .utils import accept_cookies, go_to_account, login
 
 
+MAX_WAIT = 30
+
+
 def search(browser, assert_, realistic_questions):
     icon = browser.find_element_by_xpath("//i[contains(text(), 'menu')]")
     icon.click()
 
     try:
-        forum_button = WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, "Search database"))
+        forum_button = WebDriverWait(browser, MAX_WAIT).until(
+            EC.element_to_be_clickable(
+                (By.PARTIAL_LINK_TEXT, "Search database")
+            )
         )
         time.sleep(1)
         forum_button.click()
@@ -28,11 +32,11 @@ def search(browser, assert_, realistic_questions):
     # Search
     inputbox = browser.find_element_by_id("search-bar")
     inputbox.send_keys(realistic_questions[0].title)
-    inputbox.send_keys(Keys.ENTER)
+    browser.find_element_by_id("search-button").click()
 
     # Assert question in results
     try:
-        element = WebDriverWait(browser, 5).until(
+        element = WebDriverWait(browser, MAX_WAIT).until(
             EC.presence_of_element_located((By.ID, "1"))
         )
     except NoSuchElementException:
@@ -89,11 +93,11 @@ def search(browser, assert_, realistic_questions):
     # Check re-search
     inputbox.clear()
     inputbox.send_keys(realistic_questions[1].title)
-    inputbox.send_keys(Keys.ENTER)
+    browser.find_element_by_id("search-button").click()
 
     # Assert question in results
     try:
-        element = WebDriverWait(browser, 5).until(
+        element = WebDriverWait(browser, MAX_WAIT).until(
             EC.presence_of_element_located((By.ID, "2"))
         )
     except NoSuchElementException:
@@ -102,7 +106,7 @@ def search(browser, assert_, realistic_questions):
     # Check only valid questions are searchable?
     inputbox.clear()
     inputbox.send_keys(realistic_questions[-1].title)
-    inputbox.send_keys(Keys.ENTER)
+    browser.find_element_by_id("search-button").click()
 
     # Assert question not in results
     browser.wait_for(
@@ -124,10 +128,7 @@ def check_favourites(browser, teacher, question, assignment):
         "Update Assignment" in browser.find_elements_by_tag_name("h1")[0].text
     )
 
-    fav_questions = browser.find_element_by_id("favourite-questions")
-    fav_questions.click()
-
-    time.sleep(1)
+    fav_questions = browser.find_element_by_id("search-db-app")
     assert question.text in fav_questions.text
 
 
